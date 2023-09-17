fb616cf1259549b8b2a43472c9e08e6970cd1bd0
unknown <ola.reda.900@gmail.com>
2023-09-17 19:02:01 +0300

    Add mew challenge

---
+++ script.js
@@ -0,0 +1,23 @@
+const section = document.querySelector('.image')
+const text = document.querySelector('.text')
+
+let load = 0
+
+let int = setInterval(() => {
+    bulr()
+}, 30);
+
+function bulr () {
+    load++
+    if(load === 100){
+        clearInterval(int)
+    }
+    text.innerHTML = `${load}%`
+    text.style.opacity = scale(load, 0, 100, 1, 0)
+    section.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
